dbbd6f83b120f45dcee3f0b7f93d3dfbcad34cbf
Jason Huynh <jhbeautysec@gmail.com>
2023-10-28 12:53:39 -0700

    Deleted deprecated windows scripts

--- windows10/secpolreimport.ps1
+++
@@ -1,2 +0,0 @@
-secpol | out-file c:\temp\secpol.cfg -Force
-secedit /configure /db c:\windows\security\local.sdb /cfg c:\temp\secpol.cfg /areas SECURITYPOLICY
--- windows10/userConfig.ps1
+++
@@ -1,5 +0,0 @@
-Install-Module -Name ActiveDirectory -Force -Confirm:$false
-Get-ADUser -Filter -Properties | export-csv c:\ADusers.csv
-
-
-#needs active directy on
