1fb854a13ffa445e23819dd55aac96baf57fac1d
Jason Huynh <jhbeautysec@gmail.com>
2023-10-28 17:02:19 -0700

    Added Administrator automation

---
+++ Windows10/Administrators/runAdministrators.ps1
@@ -0,0 +1,19 @@
+net localgroup Administrators | Out-File -FilePath .\Windows10\Administrators\currentAdministrators.txt
+Get-Content .\Windows10\Administrators\currentAdministrators.txt | sort | get-unique > .\Windows10\Administrators\sortedCurrentAdministrators.txt
+Get-Content .\Windows10\Administrators\competitionAdministrators.txt | sort | get-unique > .\Windows10\Administrators\sortedCompetitionAdministrators.txt
+$file2 = Get-Content -Path .\Windows10\Administrators\sortedCompetitionAdministrators.txt
+$diff = Get-Content -Path .\Windows10\Administrators\sortedCurrentAdministrators.txt | Where-Object {$_ -notin $file2}
+$diff | Out-File -FilePath .\Windows10\Administrators\naughtyAdmins.txt
+
+function removeAdmin($admins) {
+    $removeAdmin = Read-Host "Would you like to remove the folwing admins as an admin? [y/n]"
+    if ($removeAdmin -match '^[Yy]') {
+        Remove-LocalGroupMember -Group "Administrators" -Member $admins
+        Write-Host "The admins were removed."
+    }
+}
+
+$admins = Get-Content -Path '.\Windows10\Administrators\naughtyAdmins.txt'
+removeAdmin($admins)
+
+
