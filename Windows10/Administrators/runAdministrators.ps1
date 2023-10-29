c93644ca008b032544766e334d9367b931833186
Jason Huynh <jhbeautysec@gmail.com>
2023-10-28 17:08:40 -0700

    Fixed runAdministrators Sort-Object syntax

--- Windows10/Administrators/runAdministrators.ps1
+++ Windows10/Administrators/runAdministrators.ps1
@@ -1,6 +1,6 @@
 net localgroup Administrators | Out-File -FilePath .\Windows10\Administrators\currentAdministrators.txt
-Get-Content .\Windows10\Administrators\currentAdministrators.txt | sort | get-unique > .\Windows10\Administrators\sortedCurrentAdministrators.txt
-Get-Content .\Windows10\Administrators\competitionAdministrators.txt | sort | get-unique > .\Windows10\Administrators\sortedCompetitionAdministrators.txt
+Get-Content .\Windows10\Administrators\currentAdministrators.txt | Sort-Object | get-unique > .\Windows10\Administrators\sortedCurrentAdministrators.txt
+Get-Content .\Windows10\Administrators\competitionAdministrators.txt | Sort-Object | get-unique > .\Windows10\Administrators\sortedCompetitionAdministrators.txt
 $file2 = Get-Content -Path .\Windows10\Administrators\sortedCompetitionAdministrators.txt
 $diff = Get-Content -Path .\Windows10\Administrators\sortedCurrentAdministrators.txt | Where-Object {$_ -notin $file2}
 $diff | Out-File -FilePath .\Windows10\Administrators\naughtyAdmins.txt
